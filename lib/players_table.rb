957929259ee489eab2cd920b8f8aa1f18a0ed772
Jordan <jordan.thiercelin@gmail.com>
2024-12-09 15:49:09 +0100

    add comments in players_table

--- lib/players_table.rb
+++ lib/players_table.rb
@@ -13,6 +13,7 @@ class PlayersTable
   # Initializes PlayersTable to validate if there are any invalid data in the CSV,
   # transforms specific columns into integers, sorts the players first by age and then by ELO,
   # and groups them by age to create an array that is easy to use for finding champions.
+  # the method is speed because is
   def initialize(args)
     @players = args[:csv]
     @champions = []
@@ -25,9 +26,11 @@ class PlayersTable
 
     @players.each do |age, players_by_age|
       if players_by_age.first['elo'] > max_elo
+        # Shifting the first element as it is never needed again; this provides a slight optimization.
         max_elo = players_by_age.first['elo']
         @champions << players_by_age.shift
 
+        # This is useful when a player has the same elo and age, as it allows selecting them as a champion as well.
         players_by_age.each do |player|
           break if player['elo'] != max_elo
           @champions << player
@@ -55,6 +58,7 @@ class PlayersTable
     validate_elo(player)
   end
   
+  # Verify that the participant is not too young or too old.
   def validate_age(player)
     if player['age'] > Config::Variables::MAX_AGE || player['age'] < Config::Variables::MIN_AGE
       warn "Player #{player['name']} has an invalid age"
@@ -62,6 +66,7 @@ class PlayersTable
     end
   end
   
+  # Verify that the participant does not have an invalid elo.
   def validate_elo(player)
     if player['elo'] < Config::Variables::MINIMUM_ELO
       warn "Player #{player['name']} has an invalid elo. (Too low)"
@@ -69,6 +74,7 @@ class PlayersTable
     end
   end
 
+  # Validates that all mandatory headers are present in the CSV.
   def validate_headers
     missing_headers = Config::Variables::MANDATORY_HEADERS.reject do |header|
       @players.headers.include?(header)
@@ -79,6 +85,7 @@ class PlayersTable
     end
   end
 
+  # Validates headers, transforms integer columns, and verifies player data (age, elo).
   def verify_and_transform_data
     validate_headers
 
